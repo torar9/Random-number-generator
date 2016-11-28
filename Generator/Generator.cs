992c25777b08bd8f93ca709ff5c1bcba14317397
Tomáš Silber <trollinaction@gmail.com>
2016-11-28 20:24:33 +0100

    Project start

---
+++ Generator/Generator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows;
+
+namespace Generator
+{
+    class GeneratorE
+    {
+        private int lowAmout;
+        private int highAmout;
+        private int finalNumber;
+        private const sbyte maxNumber = 30;
+        private sbyte numberAmout;
+        private string message = "";
+        private Random random;
+
+        public GeneratorE(string lowAmout, string highAmout, string numberAmout)
+        {
+            try
+            {
+                this.lowAmout   = int.Parse(lowAmout);
+                this.highAmout   = int.Parse(highAmout);
+                this.numberAmout = sbyte.Parse(numberAmout);
+
+                if (this.numberAmout < 0)
+                    this.numberAmout *= -1;
+                if (this.numberAmout > maxNumber)
+                    this.numberAmout = maxNumber;
+
+
+                random = new Random();
+            }
+
+            catch
+            {
+                MessageBox.Show("The entered parameters are too big!");
+            }
+        }
+
+        public string Generate()
+        {
+            if (highAmout < lowAmout)
+            { 
+                MessageBox.Show("Mistakenly entered the upper limit!");
+                return 0.ToString();
+            }
+
+            else
+            {
+                while(numberAmout != 0)
+                {
+                    finalNumber = random.Next(lowAmout, highAmout + 1);
+                    message += finalNumber.ToString() + "  ";
+                    
+                    numberAmout--;
+                }
+
+                return message;
+            }
+        }
+
+    }
+}
---
+++ Generator/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace Generator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            GeneratedNumber.Text = "0";
+            NumberAmout.Text     = "1";
+        }
+
+        private void GenNumber(object sender, RoutedEventArgs e)
+        {
+            GeneratorE generator = new GeneratorE(lowAmout.Text, highAmout.Text, NumberAmout.Text);
+
+            GeneratedNumber.Text = generator.Generate(); //Zápis vygenerovaného číslo(generator vrací int, textbox požaduje string)
+        }
+
+        private void WAbout(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Author: Tomáš Silber"); // Naposled upraveno 10.7.2015 16:45
+        }
+
+        
+    }
+}
